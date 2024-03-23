67423253249f60cbd3d4ea3df0b81bd0c9df2e41
unajafov <ulvinajafov@ailab.az>
2024-03-24 00:26:05 +0400

    Producer refactored for kafka streams

--- src/main/java/az/najafov/kafkaemailcollector/kafka/MailEventConsumerService.java
+++ src/main/java/az/najafov/kafkaemailcollector/kafka/MailEventConsumerService.java
@@ -4,10 +4,22 @@ import az.najafov.kafkaemailcollector.model.Email;
 import az.najafov.kafkaemailcollector.model.UserRegistrationEvent;
 import az.najafov.kafkaemailcollector.repository.EmailRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,22 +28,44 @@ public class MailEventConsumerService {
 
     private final EmailRepository emailRepository;
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
-    @KafkaListener(topics = "userRegistrationTopic", groupId = "mail-info-collector")
-    public void listen(String message) {
-        try {
-            UserRegistrationEvent event = objectMapper.readValue(message, UserRegistrationEvent.class);
-            Email emailEntity = new Email();
-            String domain = event.getEmail().substring(event.getEmail().indexOf('@') + 1);
 
-            emailEntity.setEmailAddress(event.getEmail());
-            emailEntity.setDomain(domain);
+    @PostConstruct
+    public void processStream() {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "mail-info-stream-processor");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
-            emailRepository.save(emailEntity);
-            System.out.println("Processed: " + event.getEmail());
-        } catch (Exception e) {
-            log.error("Error occurred while trying to listen an event.", e.getCause());
-        }
+        StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> sourceStream = builder.stream("userRegistrationTopic");
+
+        sourceStream.flatMapValues(value -> {
+            List<Email> result = new ArrayList<>();
+            try {
+                UserRegistrationEvent event = objectMapper.readValue(value, UserRegistrationEvent.class);
+                String domain = event.getEmail().substring(event.getEmail().indexOf('@') + 1);
+                Email emailEntity = new Email();
+                emailEntity.setEmailAddress(event.getEmail());
+                emailEntity.setDomain(domain);
+                CompletableFuture.supplyAsync(() -> emailRepository.save(emailEntity), executorService)
+                        .thenAccept(email -> log.info("EVENT PROCESSED SUCCESSFULLY"))
+                        .exceptionally(e -> {
+                            log.error("Error occurred during database operation", e);
+                            return null;
+                        });
+            } catch (Exception e) {
+                log.error("Error occurred during processing stream", e);
+            }
+            return result;
+        }).foreach((key, value) -> log.info("Completed processing for key: {}", key));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
     }
 
 }
